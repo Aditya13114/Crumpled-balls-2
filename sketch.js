5ea0e27a343112f26faae6d386e1239ac228e220
Aditya13114 <75022510+Aditya13114@users.noreply.github.com>
2020-12-18 21:08:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+ dustbinImage = loadImage("dustbingreen.png");
+
+
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+	paper = new Paper(57,505,20);
+	ground = new Ground(400,690,800,20);
+	lrect = new Box(466,576,20,200);
+	rrect = new Box(666,576,20,200);
+	brect = new Box(565,674,200,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+	
+  
+  background(0);
+  text(mouseX+","+mouseY,mouseX,mouseY);
+  paper.display();
+  ground.display();
+  lrect.display();
+  rrect.display();
+  brect.display();
+  push();
+  image(dustbinImage,567,558,250,250);
+  
+ pop();
+}
+function keyPressed(){
+	if (keyCode===UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:45,y:-85})
+		//to decrease force toward x direction reduce x:
+	}
+}
+
+
+
